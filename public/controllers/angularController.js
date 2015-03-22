735661356699e7a9e2b9fddab763319cdfce4d03
tommasobratto <tbratto22@gmail.com>
2015-03-22 23:41:21 +0000

    tests unfinished, add task feature to fix

--- public/controllers/angularController.js
+++ public/controllers/angularController.js
@@ -1,11 +1,16 @@
 TodoApp.controller('TodoAppController', function($scope) {
   $scope.todoList = [];
-  $scope.todoTask = '';
+  $scope.todoTask;
+  $scope.key = {};
 
   $scope.addTask = function() {
-    if($scope.todoTask) {
-      $scope.todoList.push({text: $scope.todoTask});
+    if($scope.todoTask && $scope.key === 13) {
+      $scope.todoList.push({text: $scope.todoTask, done: false});
       $scope.todoTask = '';
     }
+  };
+
+  $scope.toggleTick = function() {
+
   };
 });
--- test/todo-unit-spec.js
+++ test/todo-unit-spec.js
@@ -13,14 +13,23 @@ describe('TodoAppController', function() {
 
   it('initialises with an empty list and a form', function() {
     expect(scope.todoList).toEqual([]);
-    expect(scope.todoTask).toEqual('');
+    expect(scope.todoTask).toBeUndefined;
   });
 
   it('should add a task', function() {
     scope.todoTask = 'testing... 1, 2, 3';
+    scope.key = 13;
     scope.addTask();
     expect(scope.todoList[0].text).toEqual('testing... 1, 2, 3');
     expect(scope.todoTask).toEqual('');
   });
+
+  it('should tick off a completed taks', function() {
+    scope.todoTask = 'testing...1, 2, 3';
+    scope.keyPress();
+    scope.addTask();
+    scope.toggleTick();
+    expect(scope.todoTask.done).toEqual(true)
+  }); 
 
 });

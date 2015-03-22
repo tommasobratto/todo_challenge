e37a8d0dacce64eb8f822a18d0aed627e8f04bfb
tommasobratto <tbratto22@gmail.com>
2015-03-22 20:22:54 +0000

    Protractor testing env setup done

--- test/spec.js
+++ test/spec.js
@@ -1,7 +1,34 @@
-describe('Protractor Demo App', function() {
-  it('should have a title', function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
+// Protractor tutorial part 2
 
-    expect(browser.getTitle()).toEqual('Super Calculator')
+describe('Protractor Demo App', function() {
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'))
+  var latestResult = element(by.binding('latest'));
+
+  beforeEach(function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+  });
+
+  it('should have a title', function() {
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
+
+  it('should add one and two', function() {
+    firstNumber.sendKeys(1);
+    secondNumber.sendKeys(2);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('3');
+  });
+
+  it('should add four and six', function() {
+    firstNumber.sendKeys(4);
+    secondNumber.sendKeys(6);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('10');
   });
 });
